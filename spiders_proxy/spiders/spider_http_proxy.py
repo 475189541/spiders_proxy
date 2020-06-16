60d3656f18a3f029023db997037ba814b057f5fd
啃西瓜我最快 <475189541@qq.com>
2020-06-16 14:56:51 +0800

    更新代理池页面js解密算法

--- spiders_proxy/spiders/spider_http_proxy.py
+++ spiders_proxy/spiders/spider_http_proxy.py
@@ -22,22 +22,23 @@ class SpidersHttpProxySpider(scrapy.Spider):
     def __init__(self, **kwargs):
         super(SpidersHttpProxySpider, self).__init__()
         self.httpbin_url = 'http://httpbin.org/ip'
+        self.httpsbin_url = 'https://httpbin.org/ip'
         self.redis_connection = redis.Redis(host=redis_host, password=redis_password, port=redis_port, db=1)
+        self.data5u_code_table = {chr(c): str(i) for i, c in enumerate(list(range(65, 91)))}
 
     def analysis_proxy(self, ip_type, ip, port):
         proxy = '%s://%s:%s' % (ip_type, ip, port)
-        request = scrapy.Request(url=self.httpbin_url, dont_filter=True, callback=self.parse_analysis, meta={'proxy': proxy})
+        url = self.httpsbin_url if ip_type == 'https' else self.httpbin_url
+        request = scrapy.Request(url=url, dont_filter=True, callback=self.parse_analysis, meta={'proxy': proxy})
         return request
 
     def start_requests(self):
         yield scrapy.Request(url=self.httpbin_url, dont_filter=True, callback=self.parse_redis_analysis)
-        xici_url_list = [['https://www.xicidaili.com/nn/%s' % i, self.parse_xicidaili] for i in range(1, 3924)]
+        xici_url_list = [['https://www.xicidaili.com/nn/%s' % i, self.parse_xicidaili] for i in range(1, 3)]
         url_list = [
             ['http://www.data5u.com', self.parse_data5u],
-            ['http://www.66daili.cn/showProxySingle/4588/', self.parse_66ip],
-            ['http://www.66daili.cn/showProxySingle/4587/', self.parse_66ip],
             ['http://www.goubanjia.com/', self.parse_goubanjia],
-        ]
+        ] + [[f'http://www.66daili.cn/showProxySingle/{i}/', self.parse_66ip] for i in range(6125, 10, -1)]
         for url in url_list:
             yield scrapy.Request(url=url[0], callback=url[1], dont_filter=True)
 
@@ -57,7 +58,9 @@ class SpidersHttpProxySpider(scrapy.Spider):
             for tr in tr_tree:
                 ip_type = tr.xpath('./span[4]/li/text()').extract_first().strip().lower()
                 ip = tr.xpath('./span[1]/li/text()').extract_first().strip()
-                port = tr.xpath('./span[2]/li/text()').extract_first().strip()
+                code = tr.xpath('./span[2]/li/@class').extract_first().strip().lstrip('port').strip()
+                port_dumps = int(''.join([self.data5u_code_table[c] for c in code])) / 8
+                port = int(port_dumps) if port_dumps <= 9999 else 9999
                 request = self.analysis_proxy(ip_type=ip_type, ip=ip, port=port)
                 yield request
             yield scrapy.Request(url=response.url, callback=self.parse_data5u, dont_filter=True)
@@ -70,6 +73,8 @@ class SpidersHttpProxySpider(scrapy.Spider):
                 port = result.xpath('./td[1]/text()').extract_first()
                 request = self.analysis_proxy(ip_type='http', ip=ip, port=port)
                 yield request
+                request = self.analysis_proxy(ip_type='https', ip=ip, port=port)
+                yield request
             yield scrapy.Request(url=response.url, callback=self.parse_66ip, dont_filter=True)
 
     def parse_xicidaili(self, response):
@@ -77,7 +82,7 @@ class SpidersHttpProxySpider(scrapy.Spider):
             tr_list = response.xpath('//table[@id="ip_list"]/tr[@class]')
             for tr in tr_list:
                 ip = tr.xpath('./td[2]/text()').extract_first()
-                port = tr.xpath('./td[2]/text()').extract_first()
+                port = tr.xpath('./td[3]/text()').extract_first()
                 ip_type = tr.xpath('./td[6]/text()').extract_first().lower()
                 request = self.analysis_proxy(ip_type=ip_type, ip=ip, port=port)
                 yield request
@@ -85,15 +90,15 @@ class SpidersHttpProxySpider(scrapy.Spider):
 
     def parse_goubanjia(self, response):
         if response.status not in self.custom_settings['HTTPERROR_ALLOWED_CODES']:
-            tr_tree = response.xpath('//tr[@class="warning"]')
+            tr_tree = response.xpath('//table[@class="table table-hover"]/tbody/tr')
             for tr in tr_tree:
                 ip_type = ''.join(tr.xpath('./td[3]//text()').extract()).strip().lower()
-                td_text = ''.join(tr.xpath('./td[1]//text()').extract()).strip().split(':')
-                if len(td_text) > 1:
-                    ip = td_text[0]
-                    port = td_text[1]
-                    request = self.analysis_proxy(ip_type=ip_type, ip=ip, port=port)
-                    yield request
+                ip = ''.join(map(lambda x: x.xpath('./text()').extract_first(''), filter(lambda f: not f.xpath('./@style').extract_first('').endswith('none;') and not f.xpath('./@class').extract_first('').startswith('port'), tr.xpath('./td[@class="ip"]/*'))))
+                code = tr.xpath('./td[@class="ip"]/span[starts-with(@class, "port")]/@class').extract_first().strip().lstrip('port').strip()
+                port_dumps = int(''.join([self.data5u_code_table[c] for c in code])) / 8
+                port = int(port_dumps) if port_dumps <= 9999 else 9999
+                request = self.analysis_proxy(ip_type=ip_type, ip=ip, port=port)
+                yield request
             yield scrapy.Request(url=response.url, callback=self.parse_goubanjia, dont_filter=True)
 
     def parse_analysis(self, response):
